f9020c1be98ec5b4c9d8cbfc0ab216bbca126e95
xiaotiandada <istianlei@qq.com>
2021-07-16 17:37:57 +0800

    feat: upload api info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,11 +10,15 @@ export class AppController {
     code: number;
     message: string;
     doc: string;
+    version: string;
+    node_env: string;
   } {
     return {
       code: 0,
       message: this.appService.getHello(),
       doc: '/openapi',
+      version: process.env.npm_package_version || '',
+      node_env: process.env.NODE_ENV,
     };
   }
 }
